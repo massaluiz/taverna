1d83dc8ab38fe1ca12cf1f00727f5c5ae20fe789
Luiz Massa <luimassapontes@gmail.com>
2020-05-15 19:46:31 -0300

    Adicionando configurações globais e baseUrl para chamadas via Axios.

---
+++ src/axios.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api.open5e.com/';
+
+export default axios;
--- src/components/classes/Classes.vue
+++ src/components/classes/Classes.vue
@@ -4,43 +4,27 @@
         <v-col
           v-for="card in cards"
           :key="card.title"
-          :cols="card.flex"
-        >
+          :cols="card.flex">
           <v-card>
             <v-img
               :src="card.src"
               class="white--text align-end"
               gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-              height="500px"
-            >
+              height="500px">
               <v-card-title v-text="card.title"></v-card-title>
             </v-img>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
-
-              <v-btn icon>
-                <v-icon>mdi-bookmark</v-icon>
-              </v-btn>
-
-              <v-btn icon>
-                <v-icon>mdi-share-variant</v-icon>
-              </v-btn>
-            </v-card-actions>
           </v-card>
         </v-col>
       </v-row>
+      <v-tabs>
+        <v-tab v-for="c of classes" :key="c.name">{{ c.name }}</v-tab>
+      </v-tabs>
     </v-container>
 </template>
 
 <script>
 
-  import axios from 'axios';
-  import config from '../../config/config'
+  import axios from '../../axios';
 
   export default {
     name: 'Classes',
@@ -55,7 +39,7 @@
     }),
     created() {
 
-      axios.get(`${config.apiURL}classes/`).then((res) => {
+      axios.get(`classes/`).then((res) => {
             this.classes = res.data.results;
             console.log(res.data.results);
         });
