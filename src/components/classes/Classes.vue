389f42aa81e7a2a78907b837f609007cd7500242
Luiz Massa <luimassapontes@gmail.com>
2020-05-15 18:12:03 -0300

    Organização dos componentes e algumas customizações de estilo.

---
+++ src/components/classes/Classes.vue
@@ -0,0 +1,65 @@
+<template>
+    <v-container fluid>
+      <v-row dense>
+        <v-col
+          v-for="card in cards"
+          :key="card.title"
+          :cols="card.flex"
+        >
+          <v-card>
+            <v-img
+              :src="card.src"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="500px"
+            >
+              <v-card-title v-text="card.title"></v-card-title>
+            </v-img>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+              <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-bookmark</v-icon>
+              </v-btn>
+
+              <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+</template>
+
+<script>
+
+  import axios from 'axios';
+  import config from './../config/config'
+
+  export default {
+    name: 'Classes',
+    data: () => ({
+      cards: [
+        { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 3 },
+        { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 3 },
+        { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3 },
+        { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3 },
+      ],
+      classes: []
+    }),
+    created() {
+
+      axios.get(`${config.apiURL}classes/`).then((res) => {
+            this.classes = res.data.results;
+            console.log(res.data.results);
+        });
+
+    }
+  }
+</script>
---
+++ src/components/menu/Menu.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-item-group>
+    <v-container>
+      <v-row>
+        <v-col>
+            <v-item>
+                <v-card color="primary" class="d-flex align-center" dark height="200">
+                    <v-scroll-y-transition>
+                        <div class="display-3 flex-grow-1 text-center">Classes</div>
+                    </v-scroll-y-transition>
+                </v-card>
+            </v-item>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-item-group>
+</template>
+
+<script>
+export default {
+    name: "Menu"
+}
+</script>
