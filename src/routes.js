5404e147009639072091510674b659c0a9f1dfed
Luiz Massa <luimassapontes@gmail.com>
2020-05-17 03:56:49 -0300

    Adicionando comportamento para quando rotas que não existem forem acessadas permanecer na home.

--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,6 @@ import ClassesDetail from './components/classes/ClassesDetail.vue';
 export const routes = [
     {path:'', name: 'menu', component: Menu},
     {path:'/classes', name:"classes", component: Classes},
-    {path:'/classes/:class', name: 'classDetail', component: ClassesDetail}
+    {path:'/classes/:class', name: 'classDetail', component: ClassesDetail},
+    {path:'*', component: Menu}
 ];
